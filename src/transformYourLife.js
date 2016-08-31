0c006d300e22fec0c4e1369749f09e37d3bf9ebb
Ryan Perez <Ryan Perez>
2016-08-30 19:11:06 -0700

    Working on Transfrom JS File

--- src/transformYourLife.js
+++ src/transformYourLife.js
@@ -13,16 +13,39 @@ var loop = function(collection, callback){
 
 
 // 1. Build transform.
-
+var transform = function(collection, callback){
+	var results = [];
+	loop(collection, function(element){
+	  results.push(callback(element));
+    });
+	return results;
+}
 
 // 2. allNumbersMultipliedByThree
+var multByThree = function(num){
+	return num*3;
+}
+
 var numbers = [1, 2, 3, 4, 5];
 
+var allNumbersMultipliedByThree = transform(numbers,multByThree);
+console.log(allNumbersMultipliedByThree);
 
 // 3. bestSentenceToUpperCase
+var capitalizeLetters = function(string){
+	return string.toUpperCase()
+}
 var bestSentence = "This is the best six week course ever!";
+var bestSentenceToUpperCase = transform(bestSentence,capitalizeLetters);
 
+console.log(bestSentenceToUpperCase);
 // 4. collectedContents
+var collectedContents = function(collection){
+	loop(collection,function(keys){
+
+	})
+}
+
 var person = {name: 'Jon', greatestFear: 'fearItself'};
 
 // 5. multByThree

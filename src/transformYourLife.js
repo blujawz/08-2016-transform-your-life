b563703dfac47d3bfcb583f437ee6c3c6d03835a
Ryan Perez <Ryan Perez>
2016-08-30 20:29:49 -0700

    Making Progress Tranform JS File

--- src/transformYourLife.js
+++ src/transformYourLife.js
@@ -32,28 +32,55 @@ var allNumbersMultipliedByThree = transform(numbers,multByThree);
 console.log(allNumbersMultipliedByThree);
 
 // 3. bestSentenceToUpperCase
-var capitalizeLetters = function(string){
-	return string.toUpperCase()
-}
+var capitalizeLetters = function(letter){
+
+    return letter.toUpperCase()
+};
+
 var bestSentence = "This is the best six week course ever!";
-var bestSentenceToUpperCase = transform(bestSentence,capitalizeLetters);
+var bestSentenceSplit = bestSentence.split(" ")
+var bestSentenceToUpperCase = transform(bestSentenceSplit,capitalizeLetters);
 
-console.log(bestSentenceToUpperCase);
+console.log(bestSentenceToUpperCase.join(' '));
 // 4. collectedContents
-var collectedContents = function(collection){
-	loop(collection,function(keys){
-
-	})
-}
-
 var person = {name: 'Jon', greatestFear: 'fearItself'};
 
+var collectedContents = transform(person, function(value,key){
+  var newArray = [];
+  newArray.push(value,key);
+	return newArray;
+
+});
+
+
+console.log(collectedContents)// [ ['name', 'Jon'], ['greatestFear', 'fear itself'] ]
 // 5. multByThree
-
+var multByThree = function(collection){
+	return transform(collection, function(number){
+		return number * 3;
+	})
+}
+var numbers = [1, 2, 3, 4, 5];
+var multipliedByThree = multByThree(numbers);
+console.log(multipliedByThree) // [3, 6, 9, 12, 15];
 // 6. upperCase
-
+var upperCase = function(string){
+	return string.toUpperCase();
+}
+var bestSentence = "This is the best six week course ever!";
+var bestSentenceToUpperCased = upperCase(bestSentence);
+console.log(bestSentenceToUpperCased) //"THIS IS THE BEST SIX WEEK COURSE EVER!";
 // 7. contentsCollection
-
+var contentsCollection = function(person){
+	return transform(person, function(value,key){
+  	var newArray = [];
+  	newArray.push(value,key);
+	return newArray;
+	})
+}
+var person = {name: 'Jon', greatestFear: 'fearItself'};
+var collectedContents = contentsCollection(person);
+console.log(collectedContents); // ['name', 'Jon', 'greatestFear', 'fear itself'];
 // 8. multByWhatever
 
 // 9. divideByWhatever
